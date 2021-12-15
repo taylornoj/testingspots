05fb338be023eefaf9da4251f7ab4a487d7c1a4c
Taylor Noj <taylornoj@gmail.com>
2021-12-14 22:36:06 -0500

    test complete for spots

--- updateSpots.js
+++ updateSpots.js
@@ -7,12 +7,31 @@
  * @return {Array}   A Days array we can save back into state.
  */
 
-const updateSpots = function (state, appointments, id) {
 
+
+
+ const fetchFreeSpots = (state, appointments) => {
+
+  
+  const appIds = state.days.filter(day => day.name === state.day);
+  const todayApp = appIds[0].appointments;
+  const emptyApp = todayApp.filter(app => !appointments[app].interview).length;
+  return emptyApp;
+}
+ const updateSpots = function (state, appointments, id) {
+  const days = [
+    ...state.days,
+  ]
+  const dayIndex = state.days.findIndex((day) => 
+    day.appointments.includes(id)
+  )
+  const spots = fetchFreeSpots(state, appointments)
+  const newDay = {
+    ...days[dayIndex], spots
+  }
+  days[dayIndex] = newDay
   // return days array
-  return [];
+  return days;
 };
 
 module.exports = updateSpots;
-
-
